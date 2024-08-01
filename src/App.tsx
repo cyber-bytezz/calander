224a773f2827062a88418c5fc596e0a6857c807f
Aro Barath Chandru B <chandru2021007@gmail.com>
2024-08-01 19:29:27 +0530

    Describe the changes made to src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
-import CustomCalendar from "./Components/CustomCalendar/CustomCalendar";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import DragAndDrop from "./Components/DragAndDrop/DragAndDrop";
-import CalendarSteps from "./Components/CalendarSteps/CalendarSteps";
-import { Calendar } from "./Components/Calendar";
 
 function App() {
   return (
